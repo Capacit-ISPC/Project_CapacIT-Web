0a654aa04f53d2a3ab05b563b9bf40a2debacb30
Belu Cacciamani <cacciamanibelen@gmail.com>
2024-05-09 00:08:14 -0300

    Diseño formulario de contacto

--- Front-end/CapacitApp/src/app/contact-form/contact-form.component.ts
+++ Front-end/CapacitApp/src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
+  contactForm!: FormGroup;
+
+  constructor(private FormBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.createContactForm();
+  }
+  createContactForm() {
+    this.contactForm = this.FormBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      // pendiente para  enviar los datos del formulario
+      console.log(this.contactForm.value);
+      // Lógica para enviar el formulario
+    } else {
+      // pendiente para el caso en el que el formulario no sea válido
+    }
+  }
+
 
 }
