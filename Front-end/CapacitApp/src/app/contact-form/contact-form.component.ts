c54404b0626664af461bdc417c385a483e5dd68a
Belu Cacciamani <cacciamanibelen@gmail.com>
2024-05-09 12:37:05 -0300

    agrego seccion medio de contacto

--- Front-end/CapacitApp/src/app/contact-form/contact-form.component.ts
+++ Front-end/CapacitApp/src/app/contact-form/contact-form.component.ts
@@ -18,6 +18,10 @@ export class ContactFormComponent {
     this.contactForm = this.FormBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
+      contactMethod: ['', Validators.required], // Agregado el control para el método de contacto
+      phoneType: [''], // Control adicional para el tipo de teléfono
+      phone: [''], // Control adicional para el número de teléfono
+      availability: [''], // Control adicional para el horario disponible
       subject: ['', Validators.required],
       message: ['', Validators.required],
     });
@@ -33,5 +37,23 @@ export class ContactFormComponent {
     }
   }
 
+  onChangeContactMethod() {
+    const contactMethodValue = this.contactForm.get('contactMethod')?.value;
+    if (contactMethodValue === 'phone') {
+      // Si el método de contacto es teléfono, establecer los controles adicionales como obligatorios
+      this.contactForm.get('phoneType')?.setValidators(Validators.required);
+      this.contactForm.get('phone')?.setValidators(Validators.required);
+      this.contactForm.get('availability')?.setValidators(Validators.required);
+    } else {
+      // Si el método de contacto no es teléfono, limpiar las validaciones de los controles adicionales
+      this.contactForm.get('phoneType')?.clearValidators();
+      this.contactForm.get('phone')?.clearValidators();
+      this.contactForm.get('availability')?.clearValidators();
+    }
+    // Actualizar las validaciones
+    this.contactForm.get('phoneType')?.updateValueAndValidity();
+    this.contactForm.get('phone')?.updateValueAndValidity();
+    this.contactForm.get('availability')?.updateValueAndValidity();
+  }
 
 }
