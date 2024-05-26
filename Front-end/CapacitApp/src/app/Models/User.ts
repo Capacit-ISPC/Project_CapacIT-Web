eafe1be20a354a3462de91964f77bff977748ebe
Belu Cacciamani <cacciamanibelen@gmail.com>
2024-05-26 02:01:59 -0300

    ·Agrego ruta y lastname

--- Front-end/CapacitApp/src/app/Models/User.ts
+++ Front-end/CapacitApp/src/app/Models/User.ts
@@ -2,6 +2,7 @@ export interface User {
     id: number;
     email: string;
     name: string;
+    last_name:string;
     password: string;
     is_active: boolean;
     is_staff: boolean;
--- Front-end/CapacitApp/src/app/services/course.service.ts
+++ Front-end/CapacitApp/src/app/services/course.service.ts
@@ -9,7 +9,7 @@ import { AuthService } from './auth.service';
 })
 export class CourseService {
 
-  private url:string = 'http://127.0.0.1:8000/api/capacit/cour/courses/'
+  private url:string = 'http://127.0.0.1:8000/courses/courses/'
 
   constructor(private httpClient: HttpClient, private authService: AuthService) { 
 
