0adea573b48e5a54b358fbbee1982f10c5ba088a
Belu Cacciamani <cacciamanibelen@gmail.com>
2024-06-03 21:28:18 -0300

    Mejoro accesibilidad con navegacion por teclado y responsividad

--- Front-end/CapacitApp/src/app/navbar/navbar.component.ts
+++ Front-end/CapacitApp/src/app/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs/operators';
+import { Component, HostListener } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -9,12 +8,29 @@ import { AuthService } from '../services/auth.service';
 })
 export class NavbarComponent {
 
-  constructor(private authService: AuthService){
+  isNavbarActive: boolean = false;
 
-  }
+  constructor(private authService: AuthService) {}
 
   isLoggedIn(): boolean {
     return this.authService.isLoggedIn();
   }
 
+  @HostListener('window:focusin', ['$event'])
+  onFocus(event: FocusEvent): void {
+    const target = event.target as HTMLElement;
+    if (target.tagName === 'A' && target.closest('.navbar')) {
+      this.isNavbarActive = true;
+    }
+  }
+
+  @HostListener('window:focusout', ['$event'])
+  onBlur(event: FocusEvent): void {
+    setTimeout(() => {
+      const activeElement = document.activeElement as HTMLElement;
+      if (!activeElement || !activeElement.closest('.navbar')) {
+        this.isNavbarActive = false;
+      }
+    }, 0);
+  }
 }
