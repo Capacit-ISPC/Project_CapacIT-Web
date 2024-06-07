66350394c1570f7c7e6f2d9171dd056633a34f2e
Nicolás Ramos <jnicolas.ramos10@gmail.com>
2024-06-07 06:16:31 -0300

    CRUD completo con permisos segun rol

---
+++ Front-end/CapacitApp/src/app/Models/Category.ts
@@ -0,0 +1,5 @@
+export interface Category {
+    id: number;
+    tipo: string;
+    description: string;
+  }
--- Front-end/CapacitApp/src/app/Models/Course.ts
+++ Front-end/CapacitApp/src/app/Models/Course.ts
@@ -1,12 +1,12 @@
 export interface Course {
-    id: number,
-    name: string,
-    description: string,
-    language:string,
-    technology: string,
-    level: string,
-    price: string,
-    link: string,
-    teacher_name: string
-    
+    id: number;
+    name: string;
+    description: string;
+    language: string;
+    technology: string;
+    level: string;
+    price: number; // Cambié a number para ser consistente con el tipo de datos esperado
+    link: string;
+    category: number; // Nueva clave foránea
+    tutor: number; // Nueva clave foránea
 }
